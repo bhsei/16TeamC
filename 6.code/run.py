4f6816504d525932582cd5773d7acab3e80a397d
监控视频 <960418051@qq.com>
2016-04-19 12:07:03 +0800

    添加label和输入框

--- 6.code/run.py
+++ 6.code/run.py
@@ -2,22 +2,68 @@
 
 from Tkinter import *
 
+userGroup = "userGroup"
+workerProcess = "1"
+pidPath = "./"
+logPath = "./"
+maxConn = "1024"
+rootPath = "./"
+errorPage = "404.html"
+
 def open():
-    print 'open'
+    print "open"
 
 def save():
-    print 'save'
+    print "save"
     
 def about():
-    print 'about'
+    print "about"
 
 root = Tk()
+
+root.title("Nginx配置工具")
+root.geometry("249x200")
+root.resizable(width=True, height= True)
+
 menubar = Menu(root)
 menubar.add_cascade(label="open", command=open)
 menubar.add_cascade(label="save", command=save)
 menubar.add_cascade(label="about", command=about)
 root.config(menu=menubar)
 
+userGroupLabel = Label(root, text = "用户组")
+userGroupLabel.grid(row=1, column=1)
+userGroupEntry = Entry(root, textvariable = userGroup)
+userGroupEntry.grid(row=1, column=2)
 
+workerProcessLabel = Label(root, text = "workerProcess")
+workerProcessLabel.grid(row=2, column=1)
+workerProcessEntry = Entry(root, textvariable = workerProcess)
+workerProcessEntry.grid(row=2, column=2)
+
+pidPathLabel = Label(root, text = "pidPath")
+pidPathLabel.grid(row=3, column=1)
+pidPathEntry = Entry(root, textvariable = pidPath)
+pidPathEntry.grid(row=3, column=2)
+
+rootPathLabel = Label(root, text = "rootPath")
+rootPathLabel.grid(row=4, column=1)
+rootPathEntry = Entry(root, textvariable = rootPath)
+rootPathEntry.grid(row=4, column=2)
+
+logPathLabel = Label(root, text = "logPath")
+logPathLabel.grid(row=5, column=1)
+logPathEntry = Entry(root, textvariable = logPath)
+logPathEntry.grid(row=5, column=2)
+
+maxConnLabel = Label(root, text = "maxConn")
+maxConnLabel.grid(row=6, column=1)
+maxConnEntry = Entry(root, textvariable = maxConn)
+maxConnEntry.grid(row=6, column=2)
+
+errorPageLabel = Label(root, text = "errorPage")
+errorPageLabel.grid(row=7, column=1)
+errorPageEntry = Entry(root, textvariable = errorPage)
+errorPageEntry.grid(row=7, column=2)
 
 root.mainloop()
