988e135a3768c3a927f39015530a2d3473efdce7
监控视频 <960418051@qq.com>
2016-04-29 10:25:26 +0800

    实现保存

--- 6.code/mainFrame.py
+++ 6.code/mainFrame.py
@@ -4,29 +4,30 @@ from conf.conf import *
 from Tkinter import *
 from tkFileDialog import *
 
-class MainFrame():
+
+class MainFrame:
     __root = Tk(":0")
-    
-    #私有变量，保存各个属性的值
+
+    # 私有变量，保存各个属性的值
     __userGroup = StringVar()
     __workerProcess = StringVar()
     __pidPath = StringVar()
     __logPath = StringVar()
-    __eventType = StringVar()    
+    __eventType = StringVar()
     __workerConnection = StringVar()
-    
-    #系统用户组数组
+
+    # 系统用户组数组
     __userGroups = None
-    
-    #获取系统支持的事件类型
-    __eventTypes = None 
-    
+
+    # 获取系统支持的事件类型
+    __eventTypes = None
+
     def open(self):
-        conf = Conf()#解析没有实现
+        conf = Conf()
         filePath = askopenfilename()
-        if filePath == '' :
+        if filePath == '':
             print "file not open"
-        else :
+        else:
             conf.setPath(filePath)
             conf.setUpFromFile()
             self.__userGroup.set(conf.getUser().getUser() + " " + conf.getUser().getGroup())
@@ -37,11 +38,22 @@ class MainFrame():
             self.__workerConnection.set(conf.getEvents().getWorkerConnections())
 
     def save(self):
-        print "save"
-        
+        conf = Conf()
+        conf.setUser(self.__userGroup.get())
+        conf.setWorkProcess(self.__workerProcess.get())
+        conf.setPid(self.__pidPath.get())
+        conf.setErrorLog(self.__logPath.get())
+
+        event = Events()
+        event.setUse(self.__eventType.get())
+        event.setWorkerConnections(self.__workerConnection.get())
+        conf.setEvents(event)
+
+        conf.writeToFile()
+
     def about(self):
         print "about"
-        
+
     def test(self):
         print self.__userGroup.get()
         print self.__workerProcess.get()
@@ -59,24 +71,25 @@ class MainFrame():
         filePath = askdirectory()
         if filePath != '':
             self.__logPath.set(filePath)
-            
+
     def __init__(self):
-        #通过工具类获取值
-        self.__userGroup.set("none")
-        self.__workerProcess.set("1")
-        self.__pidPath.set("./")
-        self.__logPath.set("./")
-        self.__eventType.set("none")
-        self.__workerConnection.set("65536")
-        
-        self.__userGroups = ['root','admin']
+        # 通过工具类获取值
+        self.__userGroup.set("")
+        self.__workerProcess.set("")
+        self.__pidPath.set("")
+        self.__logPath.set("")
+        self.__eventType.set("")
+        self.__workerConnection.set("")
+
+        # 需要通过工具获取
+        self.__userGroups = ['root', 'admin']
         self.__eventTypes = ['kqueue', 'epoll', 'select', 'poll', 'default']
-        
-        #图形界面
+
+        # 图形界面
         self.__root.title("Nginx配置工具")
         self.__root.geometry("300x200")
-        self.__root.resizable(width=True, height= True)
-        
+        self.__root.resizable(width=True, height=True)
+
         menubar = Menu(self.__root)
         menubar.add_command(label="打开", command=self.open)
         menubar.add_command(label="保存", command=self.save)
@@ -84,39 +97,39 @@ class MainFrame():
         menubar.add_command(label="test", command=self.test)
         self.__root.config(menu=menubar)
 
-        #用户组
+        # 用户组
         userGroupLabel = Label(self.__root, text="用户组")
         userGroupLabel.grid(row=1, column=1)
         userGroupDropList = OptionMenu(self.__root, self.__userGroup, *self.__userGroups)
         userGroupDropList.grid(row=1, column=2)
 
-        workerProcessLabel = Label(self.__root, text = "进程数")
+        workerProcessLabel = Label(self.__root, text="进程数")
         workerProcessLabel.grid(row=2, column=1)
-        workerProcessSpinbox = Spinbox(self.__root, from_ = 1, to = 16, textvariable = self.__workerProcess)
+        workerProcessSpinbox = Spinbox(self.__root, from_=1, to=16, textvariable=self.__workerProcess)
         workerProcessSpinbox.grid(row=2, column=2)
 
-        pidPathLabel = Label(self.__root, text = "PID存放路径")
+        pidPathLabel = Label(self.__root, text="PID存放路径")
         pidPathLabel.grid(row=3, column=1)
-        pidPathShowLabel = Label(self.__root, textvariable = self.__pidPath)
+        pidPathShowLabel = Label(self.__root, textvariable=self.__pidPath)
         pidPathShowLabel.grid(row=3, column=2)
-        pidPathButton = Button(self.__root, text="更改",command = self.choosePidPath)
+        pidPathButton = Button(self.__root, text="更改", command=self.choosePidPath)
         pidPathButton.grid(row=3, column=3)
-        
-        logPathLabel = Label(self.__root, text = "日志路径")
+
+        logPathLabel = Label(self.__root, text="日志路径")
         logPathLabel.grid(row=4, column=1)
-        logPathShowLabel = Label(self.__root, textvariable = self.__logPath)
+        logPathShowLabel = Label(self.__root, textvariable=self.__logPath)
         logPathShowLabel.grid(row=4, column=2)
-        logPathButton = Button(self.__root, text="更改",command = self.chooseLogPath)
+        logPathButton = Button(self.__root, text="更改", command=self.chooseLogPath)
         logPathButton.grid(row=4, column=3)
 
-        eventTypeLabel = Label(self.__root, text = "事件类型")
+        eventTypeLabel = Label(self.__root, text="事件类型")
         eventTypeLabel.grid(row=5, column=1)
         eventTypeDropList = OptionMenu(self.__root, self.__eventType, *self.__eventTypes)
         eventTypeDropList.grid(row=5, column=2)
 
-        workerConnectionLabel = Label(self.__root, text = "最大连接数")
+        workerConnectionLabel = Label(self.__root, text="最大连接数")
         workerConnectionLabel.grid(row=6, column=1)
-        workerConnectionSpinbox = Spinbox(self.__root, from_ = 1, to = 65536, textvariable = self.__workerConnection)
+        workerConnectionSpinbox = Spinbox(self.__root, from_=1, to=65536, textvariable=self.__workerConnection)
         workerConnectionSpinbox.grid(row=6, column=2)
 
     def show(self):
