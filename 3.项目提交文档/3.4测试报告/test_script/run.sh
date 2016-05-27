4c5fce45f40bbb0acc2e23a20aebacbde23d0571
yufengwei <yufengwei@sensetime.com>
2016-05-27 17:00:32 +0800

    add test script

--- 3.项目提交文档/3.4测试报告/test_script/run.sh
+++ 3.项目提交文档/3.4测试报告/test_script/run.sh
@@ -1,5 +1,5 @@
 #/usr/bin/sh
-testlist="1 10 100 1000 10000"
+testlist="10000"
 for i in $testlist;  do
     webbench -c $i --get http://localhost:8080/  > nginx_$i.result
     webbench -c $i --get http://localhost:80/  > apache_$i.result
